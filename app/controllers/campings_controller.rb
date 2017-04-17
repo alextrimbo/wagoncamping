dadd45a35dce44be81bae1dc096419e4a54d57fd
charles dupuy <cdupuy@gmail.com>
2017-04-17 19:31:40 -0300

    navbar redirect to new

--- app/controllers/campings_controller.rb
+++ app/controllers/campings_controller.rb
@@ -1,4 +1,7 @@
 class CampingsController < ApplicationController
+  before_action :set_camping, only:[:show, :edit, :update]
+
+
   def index
     @campings = current_user.campings
   end
@@ -7,10 +10,16 @@ class CampingsController < ApplicationController
   end
 
   def new
-    @camping = current_user.camping.new
+    @camping = Camping.new
   end
 
   def create
+    @camping = Camping.new(camping_params)
+   if @camping.save
+     redirect_to campings_show_path
+   else
+     render :new
+   end
   end
 
   def update
@@ -21,4 +30,19 @@ class CampingsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+
+  def set_camping
+    @camping = Camping.find(params[:id])
+  end
+
+  def camping_params
+   params.require(:camping).permit(
+     :camping_name, :description, :overview,
+     :rules, :pricing, :direction, :photo
+   )
+  end
+
 end
