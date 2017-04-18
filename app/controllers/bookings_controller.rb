e74dda1a716c1ed4f75ccc90b49005453452ec0c
charles dupuy <cdupuy@gmail.com>
2017-04-18 19:19:09 -0300

    Youhouu

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,11 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    camping = Camping.find(params[:camping_id])
-    today = Date.today
-    reservations = camping.booking.where("start_date >= ? OR end_date >= ?", today, today)
 
   end
 
   def create
-    @booking = current_user.booking.create(booking_params)
 
-    redirect_to @booking.camping, notice: "Your reservation has been created..."
   end
 
   def update
