1bb6c0ac4bac35340063faffac2f3735a6ffc66a
charles dupuy <cdupuy@gmail.com>
2017-04-19 14:07:28 -0300

    photo in cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+campings = [
+ {
+
+   photo: "http://www.seriouseats.com/images/2014/11/20141104-cocktail-party-old-fashioneds-holiday-vicky-wasik-3.jpg"
+   },
+ {
+
+   photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-daiquiri.jpg"
+   },
+ {
+
+   photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-margarita.jpg"
+   },
+ {
+
+   photo: "http://www.seriouseats.com/images/2014/11/20141101-cognac-sidecar-carey-jones.jpg"
+   },
+ {
+
+   photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-french75.jpg"
+   },
+ {
+
+   photo: "http://www.seriouseats.com/images/2015/03/twase-20150320-21.jpg"
+   },
+
+]
+campings.each { |camping| Camping.create(camping) }
